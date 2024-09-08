a8b7454811ad536b4c1d171374a6dc3efdeabce8
kirubel4 <kirubelayele2127@gmail.com>
2024-09-08 15:54:08 +0300

    guess

--- Number_Guessing/Number Guessing Game .py
+++ Number_Guessing/Number Guessing Game .py
@@ -1,35 +1,44 @@
 import random
+from art import logo
+from os import system
+
 def repeat():
-    again = input("do u want to play again type 'yes'.")
+    again = input("Do you want to play again type 'yes' or press any key. ")
     if again == "yes":
+        system('cls')
         guessing()
+    else:
+        system('cls')
+
 def compare():
     if difficulty == "hard":
         count = 5
     elif difficulty == "easy":
         count = 10
     while count != 0:
-            print(f"you have {count} remaining chance to guess the number.")
+            print(f"You have {count} remaining chance to guess the number.")
             guess = int(input("make a guess: "))
             if number == guess:
-                print(f"you got it!!!, The number was {number}")
+                print(f"You got it!!!, The number was {number}")
                 repeat()
                 break
             elif number > guess:
-                print("too low: make another guess")
+                print("Too low: make another guess")
                 count -= 1
             elif number < guess:
-                print("too high: make another guess")
+                print("Too high: make another guess")
                 count -= 1  
     else:
-        print(f"you lose: the number was {number}")
+        print(f"You lose: the number was {number}")
         repeat()
+
 def guessing():
     global number
     number = random.randint(1, 100)
-    print("<<<<<<<<<<<<<   welcome to the number Guessing Game!!  >>>>>>>>>>>>>>>>>>>")
+    print(logo)
     print("-->I'm thinking of a number between 1 and 100.")
     global difficulty
-    difficulty = input("#choose a difficulty to play the game. type 'easy' or 'hard': ")
-    compare()   
+    difficulty = input("Choose a difficulty to play the game. type 'easy' or 'hard': ")
+    compare() 
+
 guessing()
